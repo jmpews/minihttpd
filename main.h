8a3fc7e52bc2e2c8511debb3474b335e19ddaba5
jmpews <jmpews@gmail.com>
2015-11-19 02:24:48 +0800

    clean code

--- main.h
+++ main.h
@@ -500,7 +500,7 @@ void epoll_close(int epoll_fd,int fd,struct epoll_event *ev)
     if(epoll_ctl(epoll_fd, EPOLL_CTL_DEL, fd, ev)==-1)
         printf("! close epoll_ctl error.\n");
     else
-        printf("> close socket %d.",fd);
+        printf("> close socket %d.\n",fd);
     free_socket_node(fd);
 }
 
