f4a3ac6ca4da62f4a0c90a66f5ce274a7cf3eaf3
jmpews <jmpews@gmail.com>
2016-01-11 23:51:49 +0800

    update htdocs

--- htdocs/install.requirements.sh
+++ htdocs/install.requirements.sh
@@ -15,3 +15,6 @@ pyenv global 3.5.0
 
 # install jdk
 wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/8u66-b17/jdk-8u66-linux-i586.tar.gz"
+
+# 
+yum install bind-utils
